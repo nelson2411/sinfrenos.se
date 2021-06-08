79254746783b56325ab95d9e03e79164c5b10fbb
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2021-06-08 22:44:39 +0200

    Second changes

--- src/templates/Contact/ContactForm.js
+++ src/templates/Contact/ContactForm.js
@@ -5,11 +5,17 @@ class ContactForm extends Component{
 
     constructor(){
         super();
-        this.state={object:'', rum:'', kvm:'', email:'',name:'',subject:'',message:'',email_err:'',name_err:'',subject_err:'',message_err:'',return_msg:'',flag : false }
+        this.state={object:'', rum:'', kvm:'', adress:'', phone:'', postcode:'', contactperson:'', city:'', window:'', email:'',name:'',subject:'',message:'',email_err:'',name_err:'',subject_err:'',message_err:'',return_msg:'',flag : false }
 
         this.handleChangeObject = this.handleChangeObject.bind(this);
         this.handleChangeKvm = this.handleChangeKvm.bind(this);
+        this.handleChangeWindow = this.handleChangeWindow.bind(this);
+        this.handleChangePhoneNumber = this.handleChangePhoneNumber.bind(this);
+        this.handleChangeCity = this.handleChangeCity.bind(this);
+        this.handleChangePostCode = this.handleChangePostCode.bind(this);
+        this.handleChangeContact = this.handleChangeContact.bind(this);
         this.handleChangeRum = this.handleChangeRum.bind(this);
+        this.handleChangeAdress = this.handleChangeAdress.bind(this);
         this.handleChangeEmail = this.handleChangeEmail.bind(this);
          this.handleChangeName = this.handleChangeName.bind(this);
          this.handleChangeSubject = this.handleChangeSubject.bind(this);
@@ -40,7 +46,53 @@ class ContactForm extends Component{
         else 
           this.setState({kvm_err: ''});
     }
-  
+    handleChangePhoneNumber(e){
+      this.setState({phone:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({phone_err: 'Required Field'});
+        else 
+          this.setState({phone_err: ''});
+    }
+
+    handleChangeCity(e){
+      this.setState({city:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({city_err: 'Required Field'});
+        else 
+          this.setState({city_err: ''});
+    }
+    handleChangeContact(e){
+      this.setState({contactperson:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({contactperson_err: 'Required Field'});
+        else 
+          this.setState({contactperson_err: ''});
+    }
+    handleChangePostCode(e){
+      this.setState({postcode:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({postcode_err: 'Required Field'});
+        else 
+          this.setState({postcode_err: ''});
+    }
+
+
+
+    handleChangeAdress(e){
+      this.setState({adress:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({adress_err: 'Required Field'});
+        else 
+          this.setState({adress_err: ''});
+    }
+
+    handleChangeWindow(e){
+      this.setState({window:  e.target.value});
+        if(e.target.value === '') 
+          this.setState({window_err: 'Required Field'});
+        else 
+          this.setState({window_err: ''});
+    }
 
     handleChangeEmail(e){
         this.setState({email:  e.target.value});
@@ -81,8 +133,18 @@ class ContactForm extends Component{
            this.setState({object_err: 'Required Field'});
            if(this.state.rum === '') 
            this.setState({rum_err: 'Required Field'});
+           if(this.state.window === '') 
+           this.setState({window_err: 'Required Field'});
            if(this.state.kvm === '') 
            this.setState({kvm_err: 'Required Field'});
+           if(this.state.postcode === '') 
+           this.setState({postcode_err: 'Required Field'});
+           if(this.state.phone === '') 
+           this.setState({phone_err: 'Required Field'});
+           if(this.state.contactperson === '') 
+           this.setState({contactperson_err: 'Required Field'});
+           if(this.state.city === '') 
+           this.setState({city_err: 'Required Field'});
           if(this.state.name === '') 
               this.setState({name_err: 'Required Field'});
           if(this.state.email === '')
@@ -165,26 +227,56 @@ class ContactForm extends Component{
                             <span id="err">{this.state.rum_err}</span>
                         </div>
                         <div className="col-lg-12 form-group1">
-                            <input type="text" className="con-field" value={this.state.email} onChange={this.handleChangeEmail} id="user-email" placeholder="Email" />
-                            <span id="err">{this.state.email_err}</span>
+                            <p>Antal Fönster:</p>
+                            <input type="number" className="con-field" value={this.state.window} onChange={this.handleChangeWindow} id="user-window" placeholder="Antal Fönster" />
+                            <span id="err">{this.state.window_err}</span>
                         </div>
                     </div>
                     <div className="col-lg-12">
                         <div className="col-lg-12">
-                            <textarea name="comments" id="user-message" value={this.state.message} onChange={this.handleChangeMessage} rows="6" className="con-field" placeholder="Your Message"></textarea>
+                            <textarea name="comments" id="user-message" value={this.state.message} onChange={this.handleChangeMessage} rows="6" className="con-field" placeholder="Beskriv gärna dina frågar"></textarea>
                             <span id="err">{this.state.message_err}</span>
-                            <div className="submit-area"> 
-                                <button id="submit-contact" type="button" name="send" onClick={this.handleSubmit1} className="btn-alt" value="get a quote"> Send Message</button>
-                                <div id="simple-msg"></div>
-                            </div>
                         </div>
                     </div>
                     <div className="col-lg-6">
                     <div className="col-lg-12 form-group1">
-                            <input type="text" id="service" value={this.state.subject} onChange={this.handleChangeSubject} className="con-field" placeholder="Subject" />
-                            <span id="err">{this.state.subject_err}</span>
+                            <input type="text" id="name" value={this.state.name} onChange={this.handleChangeName} className="con-field" placeholder="Namm/Företag" />
+                            <span id="err">{this.state.name_err}</span>
                         </div>
+                        <div className="col-lg-12 form-group1">
+                            <input type="text" id="address" value={this.state.address} onChange={this.handleChangeAddress} className="con-field" placeholder="Adress" />
+                            <span id="err">{this.state.adress_err}</span>
                         </div>
+                        <div className="col-lg-12 form-group1">
+                            <input type="text" id="city" value={this.state.city} onChange={this.handleChangeCity} className="con-field" placeholder="Ort" />
+                            <span id="err">{this.state.city_err}</span>
+                        </div>
+                        <div className="col-lg-12 form-group1">
+                            <input type="text" className="con-field" value={this.state.email} onChange={this.handleChangeEmail} id="user-email" placeholder="E-Post" />
+                            <span id="err">{this.state.email_err}</span>
+                        </div>
+
+                        </div>
+                        <div className="col-lg-6">
+                        <div className="col-lg-12 form-group1">
+                            <input type="text" id="name" value={this.state.contactperson} onChange={this.handleChangeContact} className="con-field" placeholder="Kontaktperson/Företag" />
+                            <span id="err">{this.state.contactperson_err}</span>
+                        </div>
+                        <div className="col-lg-12 form-group1">
+                            <input type="text" id="post-code" value={this.state.postcode} onChange={this.handleChangePostCode} className="con-field" placeholder="Postnummer" />
+                            <span id="err">{this.state.postcode_err}</span>
+                        </div>
+                        <div className="col-lg-12 form-group1">
+                            <input type="tel" id="phone-number" value={this.state.phone} onChange={this.handleChangePhoneNumber} className="con-field" placeholder="Telefon" />
+                            <span id="err">{this.state.phone_err}</span>
+                        </div>
+
+                        </div>
+                        <div className="submit-area col-lg-12"> 
+                        <button id="submit-contact" type="button" name="send" onClick={this.handleSubmit1} className="btn-alt" value="get a quote"> Skicka Meddelande</button>
+                        <div id="simple-msg"></div>
+                        </div>
+
                 </div>
             </form>
         </div>
